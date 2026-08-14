08f89f333fc08a0f3cb7ed7aef633a52962ac668
aslıgny <84908128+2021-asgny@users.noreply.github.com>
2026-08-15 00:59:11 +0300

    tımestamp'i date formuna çevirme

--- 01_data_cleaning_views.sql
+++ 01_data_cleaning_views.sql
@@ -28,11 +28,10 @@ SELECT
   order_item_id,
   product_id,
   seller_id,
-  FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', shipping_limit_date) AS shipping_limit_date,
+  DATE(shipping_limit_date) AS shipping_limit_date,
   price,
   freight_value
-FROM `my-project-0526-502020.Olist.olist_order_items_dataset`; 
-
+FROM `my-project-0526-502020.Olist.olist_order_items_dataset`;
 
 
 CREATE OR REPLACE VIEW `my-project-0526-502020.Olist_Staging.olist_order_reviews_dataset` AS
@@ -40,22 +39,21 @@ SELECT
   review_id,
   order_id,
   review_score,
-  FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', review_creation_date) AS review_creation_date,
-  FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', review_answer_timestamp) AS review_answer_timestamp
-FROM `my-project-0526-502020.Olist.olist_order_reviews_dataset`;
-
+  DATE(review_creation_date) AS review_creation_date,
+  DATE(review_answer_timestamp) AS review_answer_timestamp
+FROM `my-project-0526-502020.Olist.olist_order_reviews_dataset`; 
 
 CREATE OR REPLACE VIEW `my-project-0526-502020.Olist_Staging.olist_orders_dataset` AS
 SELECT 
   order_id,
   customer_id,
   INITCAP(order_status) AS order_status,
-  FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', order_purchase_timestamp) AS order_purchase_timestamp,
-  FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', order_approved_at) AS order_approved_at,
-  FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', order_delivered_carrier_date) AS order_delivered_carrier_date,
-  FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', order_delivered_customer_date) AS order_delivered_customer_date,
-  FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', order_estimated_delivery_date) AS order_estimated_delivery_date
-FROM `my-project-0526-502020.Olist.olist_orders_dataset`; 
+  DATE(order_purchase_timestamp) AS order_purchase_timestamp,
+  DATE(order_approved_at) AS order_approved_at,
+  DATE(order_delivered_carrier_date) AS order_delivered_carrier_date,
+  DATE(order_delivered_customer_date) AS order_delivered_customer_date,
+  DATE(order_estimated_delivery_date) AS order_estimated_delivery_date
+FROM `my-project-0526-502020.Olist.olist_orders_dataset`;
 
 
 CREATE OR REPLACE VIEW `my-project-0526-502020.Olist_Staging.olist_customers_dataset` AS
